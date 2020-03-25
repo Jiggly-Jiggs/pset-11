aaa6ce57e862d7860dd1f11df12657c18b05b094
Matthias Rago <matthiasrago13@gmail.com>
2020-03-25 10:27:38 -0400

    6th commit

--- src/Exercises.java
+++ src/Exercises.java
@@ -33,8 +33,8 @@ public class Exercises {
    int middle = 0;
    int end = list.size() - 1;
 
-   //while (start <= end) {
-     middle = (start + end) / 2;
+   while (start <= end) {
+     //middle = (start + end) / 2;
      if (list.get(middle) < target) {
        start = middle + 1;
      }else if (list.get(middle) > target) {
